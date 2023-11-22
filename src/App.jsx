4c900caf55c5d9b184afdf68eb8500ef42cd704b
nikhilSrivastava <nikhil.srivastava@grappus.com>
2023-11-22 16:33:30 +0530

    first commit :tada:

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import {DetailsForm} from "./components/DetailsForm/index.jsx";
+
+function App() {
+
+    return (
+        <>
+            <DetailsForm/>
+        </>
+    )
+}
+
+export default App
---
+++ src/components/DetailsForm/index.jsx
@@ -0,0 +1,7 @@
+import styles from './DetailsForm.module.scss'
+
+export const DetailsForm = () => {
+    return <div className={styles.formContainer}>
+        <h3>Enter the child details</h3>
+    </div>
+}
